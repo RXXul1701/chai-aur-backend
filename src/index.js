07a8dae3fc400c27a186a0fc2536c4b52afdc75f
Rahul Majumder <rahulmajumder.1701@gmail.com>
2025-12-21 14:49:40 +0530

    Utils code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import dotenv from 'dotenv'
 import connectionDB from './db/index_db.js'
+import express from 'express'
 
+const app = express()
 dotenv.config()
-connectionDB()
+port = process.env.PORT || 8000
+connectionDB().then(
+    ()=>{
+        app.listen(port, ()=>{
+            console.log("Listening on port ", port)
+        })
+    }
+).catch(error)(
+    console.log(`DB CONNECTION FAILED: ${error}`)
+)
+
+
